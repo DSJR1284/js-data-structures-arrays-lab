0eecc8707a48c2e09fe752585c0ff1e325295b64
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-16 23:56:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+
+ const drivers = ['Milo', 'Otis', 'Garfield']
+
+  const destructivelyAppendDriver = function(name) { 
+      drivers.push(name) 
+} 
+
+ const destructivelyPrependDriver = function(name) {
+     drivers.unshift(name)
+ }
+
+ const destructivelyRemoveLastDriver = function(name) {
+    drivers.pop(name)
+}
+
+const destructivelyRemoveFirstDriver = function(name) {
+    drivers.shift(name)
+}
+
+const appendDriver = function(name) {
+    drivers.concat(name)
+}
